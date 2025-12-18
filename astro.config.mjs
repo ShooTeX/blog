d24d14117cefc6919f2ca964b79a9b18b8c6b9a6
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-12-18 18:17:51 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
+import db from '@astrojs/db';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: cloudflare(),
+  integrations: [db()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
